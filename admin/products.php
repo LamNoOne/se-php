91542aa0f72afd5662a8c5eb916477502db42e2a
doankietdev <doankietdev@gmail.com>
2024-03-13 01:31:08 +0700

    move add product to products.php

--- admin/products.php
+++ admin/products.php
@@ -1,6 +1,33 @@
-<?php require_once  dirname(__DIR__) . "/inc/init.php"; ?>
+<?php
+require_once  dirname(__DIR__) . "/inc/init.php";
+$conn = require_once dirname(__DIR__) . '/inc/db.php';
+$categories = Category::getAllCategories($conn);
+?>
 
 <?php require_once "./inc/components/header.php" ?>;
+
+<style>
+  .modal .modal-content {
+    border-radius: 5px;
+  }
+
+  .modal .modal-header h1 {
+    background: var(--primary-color) !important;
+    background-clip: text !important;
+    -webkit-text-fill-color: transparent !important;
+    font-weight: 700;
+    font-size: 25px;
+    letter-spacing: 2px;
+    text-transform: capitalize;
+  }
+
+  .modal .modal-header,
+  .modal .modal-body,
+  .modal .modal-footer {
+    padding-left: 32px;
+    padding-right: 32px;
+  }
+</style>
 
 <div class="page-wrapper">
   <div class="content">
@@ -10,7 +37,10 @@
         <h4>Manage your products</h4>
       </div>
       <div class="page-btn">
-        <a href="add-product.php" class="btn btn-added box-shadow"><img src="assets/img/icons/plus.svg" alt="img" class="me-1" />Add New Product</a>
+        <button href="add-product.php" class="btn btn-added box-shadow" data-bs-target="#addProductModal" data-bs-toggle="modal">
+          <img src="assets/img/icons/plus.svg" alt="img" class="me-1" />
+          Add New Product
+        </button>
       </div>
     </div>
 
@@ -103,47 +133,146 @@
   </div>
 </div>
 
+<div class="modal fade" id="addProductModal" aria-hidden="true" aria-labelledby="addProductModalLabel" tabindex="-1">
+  <div class="modal-dialog modal-xl modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="addProductModalLabel">Add New Product</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"><i class="fas fa-times"></i></button>
+      </div>
+      <div class="modal-body">
+        <form id="addProductForm" action="add-product.php" method="POST" enctype="multipart/form-data">
+          <div class="row gx-5">
+            <div class="col-lg-3 col-sm-6 col-12">
+              <div class="form-group">
+                <label>Product Name</label>
+                <input type="text" name="name" autofocus />
+              </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 col-12">
+              <div class="form-group">
+                <label>Category</label>
+                <select name="categoryId" class="select">
+                  <?php foreach ($categories as $category) : ?>
+                    <option value="<?php echo $category->id ?>">
+                      <?php echo $category->name ?>
+                    </option>
+                  <?php endforeach; ?>
+                </select>
+              </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 col-12">
+              <div class="form-group">
+                <label>Price</label>
+                <input type="number" name="price" />
+              </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 col-12">
+              <div class="form-group">
+                <label>Stock Quantity</label>
+                <input type="number" name="stockQuantity" />
+              </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 col-12">
+              <div class="form-group">
+                <label>Screen</label>
+                <input type="text" name="screen" />
+              </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 col-12">
+              <div class="form-group">
+                <label>Operating System</label>
+                <input type="text" name="operatingSystem" />
+              </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 col-12">
+              <div class="form-group">
+                <label>Processor</label>
+                <input type="text" name="processor" />
+              </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 col-12">
+              <div class="form-group">
+                <label>RAM</label>
+                <input type="number" name="ram" />
+              </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 col-12">
+              <div class="form-group">
+                <label>Storage Capacity</label>
+                <input type="number" name="storageCapacity" />
+              </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 col-12">
+              <div class="form-group">
+                <label>Weight</label>
+                <input type="number" name="weight" />
+              </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 col-12">
+              <div class="form-group">
+                <label>Battery Capacity</label>
+                <input type="number" name="batteryCapacity" />
+              </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 col-12">
+              <div class="form-group">
+                <label>Color</label>
+                <input type="text" name="color" />
+              </div>
+            </div>
+            <div class="col-lg-12">
+              <div class="form-group">
+                <label>Description</label>
+                <textarea class="form-control" name="description"></textarea>
+              </div>
+            </div>
+            <div class="col-lg-12">
+              <div class="form-group">
+                <label>Image</label>
+                <div class="preview-image-wrapper mx-auto">
+                  <div class="preview-image">
+                    <div class="image">
+                      <img>
+                    </div>
+                    <div class="content">
+                      <div class="icon">
+                        <i class="fas fa-cloud-upload-alt"></i>
+                      </div>
+                      <p class="text">No file chosen, yet!</p>
+                    </div>
+                    <div class="cancel-btn">
+                      <i class="fas fa-times"></i>
+                    </div>
+                    <p class="file-name">File name here</p>
+                    <input name="image" class="input-file" type="file">
+                  </div>
+                  <button class="choose-file-btn">Choose a image</button>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-12 mt-5">
+              <button type="submit" class="btn btn-submit me-2">Submit</button>
+              <button type="reset" class="btn btn-cancel" data-bs-dismiss="modal">Cancel</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
 <?php require_once "./inc/components/footer.php" ?>;
 
 <script>
   $(document).ready(function() {
-    $('table tbody').on('click', '#delete-btn', function() {
-      const id = $(this).data('id')
-      Swal
-        .fire(sweetalertDeleteConfirmConfig(
-          'Delete Product?',
-          'This action cannot be reverted. Are you sure?'
-        ))
-        .then(async function(result) {
-          try {
-            if (result.isConfirmed) {
-              const response = await $.ajax({
-                url: 'actions/delete-product.php',
-                type: 'POST',
-                dataType: 'json',
-                data: {
-                  id
-                },
-              })
-
-              if (response.status) {
-                window.location.replace(response.data.redirectUrl)
-              } else {
-                toastr.error(response.message)
-              }
-            }
-          } catch (error) {
-            toastr.error('Something went wrong')
-          }
-        })
-    })
-
     const DEFAULT_PAGE = 1
     const DEFAULT_LIMIT = 10
     const DEFAULT_SEARCH = ''
     const DEFAULT_SORT_BY = 'createdAt'
     const DEFAULT_ORDER = 'asc'
 
+    // handle render products to table
     const table = $('#table').DataTable({
       serverSide: true,
       bFilter: true,
@@ -273,12 +402,102 @@
       },
     })
 
-    // $('#table').on('init.dt', function() {
-    //   const isLastPage = Boolean(sessionStorage.getItem('isLastPage'))
-    //   sessionStorage.removeItem('isLastPage')
-    //   if (isLastPage) {
-    //     table.page('last').draw('page')
-    //   }
-    // })
+    // handle add product
+    $('#addProductForm').validate({
+      rules: {
+        name: {
+          required: true
+        },
+        categoryId: {
+          required: true
+        },
+        image: {
+          required: true
+        },
+        price: {
+          required: true,
+          number: true
+        },
+        stockQuantity: {
+          required: true,
+          number: true
+        },
+        ram: {
+          number: true
+        },
+        storageCapacity: {
+          number: true
+        },
+        weight: {
+          number: true
+        },
+        batteryCapacity: {
+          number: true
+        }
+      },
+    })
+    $('#addProductForm').submit(async function(event) {
+      try {
+        event.preventDefault()
+        if ($('#addProductForm').valid()) {
+          const formData = new FormData($(this)[0])
+
+          const response = await $.ajax({
+            url: 'actions/add-product.php',
+            type: 'POST',
+            dataType: 'json',
+            data: formData,
+            contentType: false,
+            processData: false,
+          })
+          if (response.status) {
+            toastr.success(response.message)
+            table.page('last').draw('page')
+          } else {
+            toastr.error(response.message)
+          }
+        }
+      } catch (error) {
+        toastr.error('Something went wrong')
+      } finally {
+        // clear form
+        $('#addProductModal').modal('hide');
+        $(this).closest('#addProductForm').find('input, textarea, select').val('')
+        $(this).closest('#addProductForm').find('select').prop('selectedIndex', 1)
+        $(this).closest('#addProductForm').find('.preview-image img').prop('src', '').hide();
+      }
+    })
+
+    // handle delete product
+    $('table tbody').on('click', '#delete-btn', function() {
+      const id = $(this).data('id')
+      Swal
+        .fire(sweetalertDeleteConfirmConfig(
+          'Delete Product?',
+          'This action cannot be reverted. Are you sure?'
+        ))
+        .then(async function(result) {
+          try {
+            if (result.isConfirmed) {
+              const response = await $.ajax({
+                url: 'actions/delete-product.php',
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                  id
+                },
+              })
+
+              if (response.status) {
+                window.location.replace(response.data.redirectUrl)
+              } else {
+                toastr.error(response.message)
+              }
+            }
+          } catch (error) {
+            toastr.error('Something went wrong')
+          }
+        })
+    })
   })
 </script>
